c8d03d7522e6e74e28730a21fd92c6f6b59a1ca2
Hans Vanpée <hans@vanpee.be>
2018-03-26 12:51:08 +0200

    Ajout exercice03

---
+++ pdo/exercice03.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file    exercice02.php
+ * @author	Hans Vanpee <hans@vanpee.fr>
+ * @brief	Complétez le formulaire de l'exercice 02 pour ajouter des lignes à
+ * une commande.
+ *
+ * Une fois la commande créée, affichez un formulaire qui permet à
+ * l'utilisateur d'ajouter des produits à la commande. Les produits sont
+ * affichés dans une balise select.
+ */
+
+
+// vim: set expandtab:
+// vim: set tabstop=4 shiftwidth=4 softtabstop=4:
